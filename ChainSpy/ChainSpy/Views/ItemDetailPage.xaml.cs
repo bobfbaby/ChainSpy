324a1921ae30f1a025261d2fa53a3d311319a041
Daniel Walas <bobfbaby@gmail.com>
2022-09-02 11:06:28 +0100

    Add project files.

---
+++ ChainSpy/ChainSpy/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ChainSpy.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ChainSpy.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
